e1d78935fffbf6fd9bcde2266fae3177c20547a0
24DP2RSter <A240335RS@rvt.lv>
2025-09-09 13:40:36 +0300

    programma ar java

---
+++ src/main/java/rvt/program.java
@@ -0,0 +1,20 @@
+package rvt;
+Package rvt;
+import java.util.Scanner;
+public class program {
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+
+        
+        
+        String message = scanner.nextLine();
+
+        
+        System.out.println(message);
+        System.out.println(message);
+    }
+}
