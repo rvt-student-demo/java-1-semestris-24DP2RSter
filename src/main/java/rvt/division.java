68f8789807bb22b0f5df6e3d82756818ef814d56
24DP2RSter <A240335RS@rvt.lv>
2025-09-16 12:47:14 +0300

    matematika ar java

---
+++ src/main/java/rvt/division.java
@@ -0,0 +1,14 @@
+package rvt;
+
+public class division {
+    //Dalot (int) ar (int),
+    //rezultāts vienmēr būs (int)
+    //vismaz vienai pusei jabūt par double
+    public static void main(String[] args) {
+    int a =7;
+    int b =2;
+        System.out.println(
+            a / Double.valueOf(b)
+    );
+    }
+}
