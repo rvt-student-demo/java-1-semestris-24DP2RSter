ff2e363c36f23a7e132036b0146b3756c8cb40b4
24DP2RSter <A240335RS@rvt.lv>
2025-12-11 16:56:34 +0200

    overloaded counter classes for value manipulation

---
+++ src/main/java/rvt/overloaded_counter_am.java
@@ -0,0 +1,38 @@
+package rvt;
+
+public class overloaded_counter_am {
+
+private int value;
+
+public overloaded_counter_am(int StartValue) {
+this.value = StartValue;
+}
+
+public overloaded_counter_am() {
+this(0);
+}
+
+public int value() {
+return this.value;
+}
+
+public void increase() {
+this.value++;
+}
+
+public void decrease() {
+this.value--;
+}
+
+public void increase(int increaseBy) {
+if (increaseBy > 0) {
+this.value += increaseBy;
+}
+}
+
+public void decrease(int decreaseBy) {
+if (decreaseBy > 0) {
+this.value -= decreaseBy;
+}
+}
+}
